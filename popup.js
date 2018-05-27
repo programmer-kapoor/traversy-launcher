7a3c46ea77ca2efbe903b5f5af965b7fcb3016f2
programmer-kapoor <shubhamkapoor9798@gmail.com>
2018-05-27 16:01:27 +0530

    version

--- popup.js
+++ popup.js
@@ -1,1 +1,32 @@
 // Future JavaScript will go here
+
+
+      function allowDrop(ev) {
+
+          ev.preventDefault();
+          // ev.dataTransfer.dropEffect = "copy"
+      }
+      
+      function drag(ev) {
+          console.log("fire");
+          console.log(ev.target.id);
+          ev.dataTransfer.setData("text", ev.target.id);
+      }
+      
+      function drop(ev) {
+          ev.preventDefault();
+          var data = ev.dataTransfer.getData("text");
+          console.log(data);
+          console.log(typeof data);
+          ev.target.appendChild(document.getElementById(data));
+      }
+      
+      function sub()
+      {
+        alert("sk");
+        var x, text;
+        // Get the value of the input field with id="numb"
+        x = document.getElementById("addLink").value;
+        console.log(x);
+        console.log(typeof x);
+    }
